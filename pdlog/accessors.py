f81e3ae01e59fd6600ba9202d843c2d5e851ac7e
seem <wasim@dataprophet.com>
2020-02-01 19:12:24 +0200

    Add more loggers & method duration
    Log assign and set index.
    
    Log args and kwargs at DEBUG level.
    
    Log duration of method call.

--- pdlog/accessors.py
+++ pdlog/accessors.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from functools import wraps
 from typing import Callable
 
@@ -25,21 +26,36 @@ class FrameLogMethods:
         @wraps(getattr(pd.DataFrame, method_name))
         def inner(self, *args, **kwargs):
 
+            before_time = datetime.now()
+
             df_method = getattr(self._data, method_name)
 
             after_df = df_method(*args, **kwargs)
 
+            after_time = datetime.now()
+            elapsed = after_time - before_time
+
             if after_hook is not None:
-                after_hook(self._data, after_df, method_name)
+                after_hook(self._data, after_df, method_name, args, kwargs, elapsed)
 
             return after_df
 
         return inner
 
 
+# TODO: Add tests
+# TODO: Use decorators instead?
+# TODO: getitem and setitem seem like a bit of a nightmare... Maybe just don't support it?
 LOGGED_METHODS = {
     "dropna": {"after_hook": logging.log_filter},
     "drop_duplicates": {"after_hook": logging.log_filter},
+    "iloc": {"after_hook": logging.log_filter},
+    "loc": {"after_hook": logging.log_filter},
+    "__getitem__": {"after_hook": logging.log_filter},
+    "query": {"after_hook": logging.log_filter},
+    "assign": {"after_hook": logging.log_assign},
+    "set_index": {"after_hook": logging.log_set_index},
+    "reset_index": {"after_hook": logging.log_set_index},
 }
 for method_name, kwargs in LOGGED_METHODS.items():
     setattr(
--- pdlog/logging.py
+++ pdlog/logging.py
@@ -1,3 +1,7 @@
+# TODO: Could we refactor something out here?
+#       E.g., having to log every time by passing in the function and elapsed time seems
+#       like an abstraction is waiting...
+# TODO: Should elapsed be DEBUG or INFO?
 import logging
 
 import pandas as pd
@@ -8,7 +12,13 @@ from .utils import percent
 logger = logging.getLogger("pdlog")
 
 
-def log_filter(before_df: pd.DataFrame, after_df: pd.DataFrame, function: str) -> None:
+def log_filter(
+    before_df: pd.DataFrame,
+    after_df: pd.DataFrame,
+    function: str,
+    function_args,
+    function_kwargs,
+) -> None:
     """Log changes in a dataframe for filter operations (dropped rows/cols)."""
 
     n_rows_before = len(before_df)
@@ -50,3 +60,47 @@ def log_filter(before_df: pd.DataFrame, after_df: pd.DataFrame, function: str) -
             percent(n_rows_dropped, n_rows_before),
             n_rows_after,
         )
+
+    # TODO: Don't print dataframe or series or numpy array args?
+    #       Instead summarize?
+    #       Would be a util func...
+    logger.debug("args: %s, kwargs: %s", function_args, function_kwargs)
+
+
+def log_assign(before_df: pd.DataFrame, after_df: pd.DataFrame, function: str,
+               function_args, function_kwargs) -> None:
+    """Log changes in a dataframe for assignment operations (added cols)."""
+
+    new_columns = [c for c in after_df.columns if c not in before_df.columns]
+
+    if new_columns:
+        logger.info("%s: added %d columns: %s", function, len(new_columns), new_columns)
+
+    # TODO: Need args?
+    # TODO: Is this necessary at all?
+    #       Kwargs are just gonna be the added columns.
+    #       And values are just gonna be lambdas...
+    function_kwargs = {k: v.__name__ if callable(v) else v
+                       for k, v in function_kwargs.items()}
+    logger.debug("args: %s, kwargs: %s", function_args, function_kwargs)
+
+
+def log_set_index(before_df: pd.DataFrame, after_df: pd.DataFrame, function: str,
+                  function_args, function_kwargs, elapsed) -> None:
+
+    before_index_name = before_df.index.name
+    before_index_type = type(before_df.index).__name__
+
+    after_index_name = after_df.index.name
+    after_index_type = type(after_df.index).__name__
+
+    # TODO: Could show first and last vals, not sure if important...
+    logger.info("%s – %ss: set from '%s' (%s) to '%s' (%s)",
+                function, elapsed.total_seconds(), before_index_name, before_index_type, after_index_name, after_index_type)
+    logger.debug("args: %s, kwargs: %s", function_args, function_kwargs)
+
+
+# def log_reset_index(before_df: pd.DataFrame, after_df: pd.DataFrame, function: str,
+#                     function_args, function_kwargs) -> None:
+
+#     logger.debug("args: %s, kwargs: %s", function_args, function_kwargs)
