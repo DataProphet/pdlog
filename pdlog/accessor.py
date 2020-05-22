af3915873d79974923f119a79c0d3ee05f4fc9e0
seem <mwlorgat@gmail.com>
2020-05-22 21:25:00 +0200

    Add missing accessor file
    Simplified logic here, but there's duplication now

---
+++ pdlog/accessor.py
@@ -0,0 +1,65 @@
+from typing import Any
+
+import pandas as pd
+
+from . import logging
+
+
+@pd.api.extensions.register_dataframe_accessor("log")
+class LogAccessor:
+    def __init__(self, data: pd.DataFrame):
+        self._data = data
+
+    def dropna(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "dropna", *args, **kwargs)
+
+    def drop_duplicates(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "drop_duplicates", *args, **kwargs)
+
+    def iloc(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "iloc", *args, **kwargs)
+
+    def loc(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "loc", *args, **kwargs)
+
+    def __getitem__(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "__getitem__", *args, **kwargs)
+
+    def query(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "query", *args, **kwargs)
+
+    def head(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "head", *args, **kwargs)
+
+    def tail(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "tail", *args, **kwargs)
+
+    def sample(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "sample", *args, **kwargs)
+
+    def drop(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_filter(self._data, "drop", *args, **kwargs)
+
+    def set_index(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_change_index(self._data, "set_index", *args, **kwargs)
+
+    def reset_index(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_change_index(self._data, "reset_index", *args, **kwargs)
+
+    def rename(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_rename(self._data, "rename", *args, **kwargs)
+
+    def pivot(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_reshape(self._data, "pivot", *args, **kwargs)
+
+    def melt(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_reshape(self._data, "pivot", *args, **kwargs)
+
+    def fillna(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_fillna(self._data, "fillna", *args, **kwargs)
+
+    def bfill(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_fillna(self._data, "bfill", *args, **kwargs)
+
+    def ffill(self, *args: Any, **kwargs: Any) -> pd.DataFrame:
+        return logging.log_fillna(self._data, "fillna", *args, **kwargs)
