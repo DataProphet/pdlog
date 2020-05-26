b43493fe95448decbcdf5b706d33b62f0170e878
seem <mwlorgat@gmail.com>
2020-05-26 21:16:30 +0200

    Update setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-#! /usr/bin/env python
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-setup(
-    name="pdlog",
-    version="0.0.1",
-    description="Logging for pandas dataframes",
-    author="Wasim Lorgat",
-    author_email="wasim@dataprophet.com",
-    packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "typing_extensions"
-    ],
-)
+setup()
